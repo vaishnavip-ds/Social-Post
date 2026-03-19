758d5ceb43f71320e45473d6f11a3eeed0d47916
Vaishnavi Pandey <shreya@Vaishnavis-MacBook-Air.local>
2026-03-19 14:30:52 +0530

    first commit

--- backend-sqlalchemy-CRUD-routes/app.py
+++ backend-sqlalchemy-CRUD-routes/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session, url_for
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from model.users import Users
 from model.users import db
+import os
 
 from form import RegisterForm
 
